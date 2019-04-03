7359425428cc80c20ad1c0e874d9980d0f4369d2
Nguyen <Administrator@PC014>
2019-04-03 16:20:46 +0700

    trang 47

---
+++ BigSchool/Controllers/FollowingsController.cs
@@ -0,0 +1,38 @@
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class FollowingsController : Controller
+    {
+        // GET: Followings
+        private readonly ApplicationDbContext _dbContext;
+        public FollowingsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeID))
+                return BadRequest("Following already exists!");
+            var folowing = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeID
+            };
+            _dbContext.Followings.Add(folowing);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
