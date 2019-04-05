42f2e7cc06e92ae7b7d85d2f7a29369e2e917dae
Nguyên Vũ Văn <hoangnguyenvuive210698@gmail.com>
2019-04-05 11:47:54 +0700

    chỉnh sửa

--- BigSchool/ViewModels/ValidTime.cs
+++ BigSchool/ViewModels/ValidTime.cs
@@ -1,23 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace BigSchool.ViewModels
 {
-    public class ValidTime : ValidationAttribute
+    internal class ValidTime : ValidationAttribute
     {
-        // GET: ValidTime
         public override bool IsValid(object value)
         {
             DateTime dateTime;
-            var isValid = DateTime.TryParseExact(Convert.ToString(value), 
-                "HH:mm", 
-                CultureInfo.CurrentCulture, 
-                DateTimeStyles.None, 
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "HH:mm",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
                 out dateTime);
             return isValid;
         }
